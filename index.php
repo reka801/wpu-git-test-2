fecb00a4116a3c86240d61644492e87a4c9e1e64
reka801 <rekadoang30@gmail.com>
2023-02-07 11:22:25 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio officiis quia ad vel quae provident, aliquid repellat doloribus earum, alias eos eius necessitatibus quasi obcaecati nesciunt! Nihil deleniti ea quasi?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
